44e9dd782d09a5490193612a91130f41203b9d01
Javier Herrera <jaaah0010@gmail.com>
2021-12-09 10:47:03 -0600

    Manejo de ruteo basico

---
+++ src/components/CRUD/index.js
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+export default function CRUD() {
+	return (
+		<div>
+			Aquí va el CRUD			
+		</div>
+	)
+}
---
+++ src/components/Layout/index.js
@@ -0,0 +1,18 @@
+import Header from "./Header";
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Footer from "./Footer";
+
+
+export default function Layout() {
+    return (
+        <>
+            <Header />
+                {/* EL OUTLET SON TODOS LOS COMPONENTES HIJOS (CHILDREN) DE LA ROUTA PADRE, DEPENDIENDO DEL PATH */}
+                {/* SI ESTOY EN http://localhost:3000/crud, ENTONCES OUTLET VA A SER <CRUD /> */}
+                <Outlet />
+
+            <Footer />
+        </>
+    )
+}
