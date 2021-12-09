dd0a3c5216f9aaf9b9376c46323b66167703ba0f
Javier Herrera <jaaah0010@gmail.com>
2021-12-09 13:09:29 -0600

    paises terminado

--- src/Router.js
+++ src/Router.js
@@ -1,34 +1,52 @@
+
+
+
 // 1. IMPORTACIONES
 import React from 'react'
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import Home from './components/Home'
 import CRUD from './components/CRUD'
 import Layout from './components/Layout'
+import Countries from './components/Countries'
+import Country from './components/Country'
+
+import data	from './countries.json'
 
 
 // 2. FUNCIÓN
 const Router = () => {
 
-    // Arquitectura de routing en react
+	// ARQUITECTURA DE ROUTING EN REACT
 	return (
-        <>
+		<>
+			<BrowserRouter>
+				<Routes>
 
-        <BrowserRouter>
-            <Routes>
-               <Route  path="/" element={<Layout />}>
-                    {/* index es usar el path de la ruta padre */}
-                    <Route index element={<Home />} />
-                    <Route path="crud" element={<CRUD />} />  
-               </Route>
-            </Routes>
-        </BrowserRouter>
+					{/* RUTA PADRE / PARENT ROUTE */}
+					<Route path="/" element={<Layout />}>
+						{/* localhost:3000/ */}
+						{/* index es usar el path de la ruta padre */}
+						<Route index element={<Home />} />
+						{/* localhost:3000/crud */}
+						<Route path="crud" element={<CRUD />} />
+						{/* localhost:3000/countries */}
+						<Route path="countries" element={<Countries allCountries={data} />}>
+							{/* CHILDREN ROUTES de /countries */}
+							{/* localhost:3000/countries/MEX */}
+							{/* localhost:3000/countries/COL */}
+							{/* localhost:3000/countries/PER */}
+							{/* localhost:3000/countries/USA */}
+							{/* localhost:3000/countries/... */}
+							<Route path=":id" element={<Country allCountries={data}/>} />
+						</Route>
 
+					</Route>
+				</Routes>
+			</BrowserRouter>
+		</>
+	)
 
-        </>
-    )
 }
-
 
 // 3. EXPORTACIÓN
 export default Router
-
